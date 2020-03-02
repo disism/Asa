2815bee010a17fad5ef1a57aed7725e4ae3542d0
disism <x@disism.com>
2020-03-02 11:24:15 +0800

    'keyword'

---
+++ src/pages/chrono.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function Chrono() {
+  return (
+    <Layout>
+      <SEO title="Chrono" />
+    </Layout>
+  )
+}
+
+export default Chrono
+
